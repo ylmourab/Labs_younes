50dab649f5859e06de38502983836ed044843e4c
Younes <ylmourab77@gmail.com>
2021-04-07 09:55:34 +0200

    Seeder test

---
+++ app/Http/Controllers/IntroController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Intro;
+use Illuminate\Http\Request;
+
+class IntroController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Intro  $intro
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Intro $intro)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Intro  $intro
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Intro $intro)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Intro  $intro
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Intro $intro)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Intro  $intro
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Intro $intro)
+    {
+        //
+    }
+}
